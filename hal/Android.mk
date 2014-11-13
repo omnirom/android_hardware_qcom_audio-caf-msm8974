3a70215b24304763238f90dae402f7d4020f6d1f
Steve Kondik <steve@cyngn.com>
2014-11-12 23:24:07 -0800

    hal: Add option to use low latency as primary output
     * Google's original design uses the low latency output as primary for
       playback unless a series of checks are passed in the framework which
       would use deep buffer mode.  Qualcomm changed this long ago in order
       for usecases like AV which don't require low latency to be forced to
       use deep buffering. This has been a catastrophic failure ever since
       it was changed, causing crackles and pops randomly while playing
       notifications due to underruns. Either I am too stupid to make it
       work, or this behavior is fundamentally flawed.
     * Add a flag to do it Google's way, I'm tired of this bug.
    
    Revert "hal: Use deep buffer output as primary"
    
    This reverts commit 8f715d94926ee51baf766800f4e9c3ceec435e4c.
    
    Change-Id: I678478798274a35fe0d117650cfd62b1a433f448

--- hal/Android.mk
+++ hal/Android.mk
@@ -122,6 +122,10 @@ ifneq ($(strip $(DOLBY_DDP)),true)
 endif
 endif
 
+ifeq ($(AUDIO_FEATURE_LOW_LATENCY_PRIMARY),true)
+    LOCAL_CFLAGS += -DLOW_LATENCY_PRIMARY
+endif
+
 ifeq ($(string $(AUDIO_FEATURE_ENABLED_LOW_LATENCY_CAPTURE)),true)
     LOCAL_CFLAGS += -DLOW_LATENCY_CAPTURE_USE_CASE=1
 endif
