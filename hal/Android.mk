omnirom/android_hardware_qcom_audio-caf-bfam

hal: add i2s speaker compile flag
this is actually only a workaround. other hal versions
(e.g. AOSP or CAF for shamu) support setting backend devices
using audio_platform_info.xml file.

But just for that we wont backport this thing right now
cause we only need it for oppo n3 at the moment

Change-Id: Icc3ceda216af15df9d0caaa7c176bb6d14f9e68a

hal/Android.mk
@@ -169,6 +169,10 @@ ifeq ($(strip $(AUDIO_FEATURE_DISABLED_HWDEP_CAL)),true)
     LOCAL_CFLAGS += -DHWDEP_CAL_DISABLED
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_ENABLE_I2S_SPEAKER)),true)
+    LOCAL_CFLAGS += -DI2S_SPEAKER_ENABLED
+endif
+
 LOCAL_COPY_HEADERS_TO   := mm-audio
 LOCAL_COPY_HEADERS      := audio_extn/audio_defs.h
 

omnirom/android_hardware_qcom_audio-caf-msm8974

hal/Android.mk
@@ -169,6 +169,10 @@ ifeq ($(strip $(AUDIO_FEATURE_DISABLED_HWDEP_CAL)),true)
     LOCAL_CFLAGS += -DHWDEP_CAL_DISABLED
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_I2S_SPEAKER_ENABLED)),true)
+    LOCAL_CFLAGS += -DI2S_SPEAKER_ENABLED
+endif
+
 LOCAL_COPY_HEADERS_TO   := mm-audio
 LOCAL_COPY_HEADERS      := audio_extn/audio_defs.h
 